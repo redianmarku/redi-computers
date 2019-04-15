44152a447699e56c5409ebf7e0ec4a8b79a77493
redianmarku <redian1marku@gmail.com>
2019-04-15 20:59:21 +0200

    redi web

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function pafunksion(){
+	alert("Ky sherbin eshte akoma nen zhvillim...")
+}
+function derguar(){
+
+	var hap = document.getElementById("#hap").value;
+
+	  if (hap = true) {
+	  	alert("Kerkesa juaj u dergua me sukses.");
+	  }
+	  else{
+	  	alert("no");
+	  }
+	
+}
+function greenborder (){
+
+	document.getElementById('kyqu').style.border = "4px solid red";
+}
